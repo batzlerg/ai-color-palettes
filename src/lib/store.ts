15ec24bc6bdd165e4658ae58f487b8b0468be276
Graham Batzler <batzler@pm.me>
2023-02-20 17:05:03 -0700

    implement LocalStorage rehydration

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,8 +1,6 @@
 import { writable } from "svelte/store";
 import type { Response } from './types';
 
-window.localStorage.getItem('color_palette_responses')
-
 // config data
 export const promptContext = writable(
   `Return a color palette in JSON that matches the mood of the prompt phrase. Include the color as a hex code and a name for the color.`);
@@ -12,6 +10,20 @@ export const exampleOutput = writable(`{
   "dark grey": "#666666",
   "white": "#ffffff",
 }`);
-export const history = writable<[string, Response][]>([]);
 
-history.subscribe((value) => localStorage.content = value)
+// initialize our previous response state backed by LS so we preserve history between page loads
+let cachedHistory = [];
+try {
+  const rawLSData = window.localStorage.getItem('color_palette_history');
+  if (rawLSData !== undefined) {
+    cachedHistory = JSON.parse(rawLSData);
+  }
+  if (cachedHistory.length) {
+    console.log(`Restored ${cachedHistory.length} history items from localStorage.`)
+  }
+} catch (err) {
+  console.error(err);
+}
+export const history = writable<[string, Response][]>(cachedHistory ?? []);
+
+history.subscribe((value) => localStorage.setItem('color_palette_history', JSON.stringify(value)));
